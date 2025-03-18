0e893c5d7591e1f1974f43f9306ea242a7a7fd20
Terell520 <terellbrown200@gmail.com>
2025-03-17 22:15:59 -0400

    Update 3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,25 +3,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Movement speed
-    public float speed 2.50f;
-    public CharacterController playerMovement;
-    public GameObject AnnaCap;
-    public Vector3 annaMov;
+    
+    public CharacterController annamov;
+    public float speed = 5f;
+    public Vector3 annaPos;
     public Rigidbody rb;
-
-     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     //Update functions as Key movement code
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) //If the if statement is true, for every frame the W key held down.
-        {
-            
-        }
+       
     }
 }
