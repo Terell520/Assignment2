ef4f49738a4b09436ed482bfac4dafa83da0f712
Terell520 <124600032+Terell520@users.noreply.github.com>
2025-03-18 18:07:54 -0400

    Update 4
    Added some code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     //Update functions as Key movement code
     void Update()
     {
-       
+        float horizontal = Input.GetAxisRaw("Horizontal"); //Raw inputs for when A & D are pressed 
+        float vertical = Input.GetAxisRaw("Vertical"); // Raw inputs for when W & S are pressed
+
     }
 }
