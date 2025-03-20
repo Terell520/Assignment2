9c803a905cbbf6a4e0a44316de1988b41012b952
Terell520 <terellbrown200@gmail.com>
2025-03-20 00:57:19 -0400

    Update 5
    added images

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,13 @@ public class PlayerMovement : MonoBehaviour
     
     public CharacterController annamov;
     public float speed = 5f;
-    public Vector3 annaPos;
-    public Rigidbody rb;
+
+    public Transform cam;
+
+    public float DaSmoothTime = 0.1f;
+
+    //serves as a reference to a veriable that holds current smooth Velocity
+    float smoothTurn; 
 
     // Update is called once per frame
     //Update functions as Key movement code
@@ -15,6 +20,17 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal"); //Raw inputs for when A & D are pressed 
         float vertical = Input.GetAxisRaw("Vertical"); // Raw inputs for when W & S are pressed
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurn, DaSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; //Turns rotaton into direction
+            annamov.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
 
     }
 }
